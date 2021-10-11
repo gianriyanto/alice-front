d085dbab6b13d02de6ccaad5c22f55163deaffd4
gianriyanto <li.griyanto@gmail.com>
2021-10-11 21:42:30 +1100

    layout adjustment to the Dashboard.vue, changed font

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="side-bar">
-    <div class="content-wrapper h-full w-full flex flex-col items-center justify-between pt-12 pb-8">
+    <div class="content-wrapper h-full w-full flex flex-col items-center justify-between pt-12 pb-12">
       <div class="logo w-full h-16 justify-center flex">
-        <div class="bg-green-300 opacity-60 rounded-xl h-12 w-12">
-        </div>
+        <div class="bg-green-300 opacity-60 rounded-xl h-12 w-12"/>
       </div>
-      <i class="ri-logout-box-r-line" style="font-size: 36px; color: #FFFFFF"></i>
+      <i class="ri-logout-box-r-line" style="font-size: 36px; color: #FFFFFF"/>
     </div>
   </div>
 </template>
--- src/components/home/Dashboard.vue
+++ src/components/home/Dashboard.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="dashboard w-full" class="">
-    <div class="content-wrapper h-full h-24 w-12/16 pl-20 flex-column">
-      <div class="header-container h-24 border-white">
-        <h2 class="pt-14 font-roboto-slab font-black text-white text-3xl"> Alice. </h2>
+  <div id="dashboard w-full" class="flex flex-row justify-center">
+    <div class="content-wrapper h-full h-24 w-10/16 flex flex-col">
+      <div class="header-container h-24">
+        <h2 class="pt-14 font-roboto-slab font-black text-white text-3xl">
+          Alice.
+        </h2>
       </div>
       <div class="search-container flex h-16 w-full mt-24">
-        <h2 class="self-center font-montserrat text-3xl font-medium tracking-tighter text-lightgrey">
+        <h2 class="self-center font-montserrat text-2xl font-normal tracking-tighter text-lightgrey">
           Search and you shall find
         </h2>
       </div>
       <div class="trending-container flex-column h-7/16 mt-16">
-        <h3 class="header h-2/16 font-montserrat text-xl font-normal tracking-tighter text-white">
+        <h3 class="header h-2/16 font-nunito-sans text-xl font-semibold tracking-tighter text-white">
           Trending
         </h3>
         <div class="trending-items h-14/16 mt-2 flex flex-wrap">
-          <span class="item bg-blue-500 opacity-70 h-24 w-24 mr-3 mt-4 rounded-md"/>
-          <span class="item bg-blue-500 opacity-70 h-24 w-24 mr-3 mt-4 rounded-md"/>
-          <span class="item bg-blue-400 opacity-70 h-24 w-24 mr-3 mt-4 rounded-md"/>
-          <span class="item bg-blue-500 opacity-70 h-24 w-24 mr-3 mt-4 rounded-md"/>
-          <span class="item bg-blue-500 opacity-70 blue h-24 w-24 mr-3 mt-4 rounded-md"/>
-          <span class="item bg-blue-500 opacity-70 h-24 w-24 mr-3 mt-4 rounded-md"/>
-          <span class="item bg-blue-500 opacity-70 h-24 w-24 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-300 opacity-60 h-20 w-20 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-500 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-500 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-500 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-500 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-500 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md"/>
+          <span class="item bg-blue-500 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md"/>
         </div>
       </div>
       <div class="explore-container flex-column h-7/16 mt-20">
-        <h3 class="header h-2/16 font-montserrat text-xl font-normal tracking-tighter text-white">
+        <h3 class="header h-2/16 font-nunito-sans text-xl font-semibold tracking-tighter text-white">
           Explore
         </h3>
         <div class="explore-tags h-14/16 mt-2 flex flex-wrap">
