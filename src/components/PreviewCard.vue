115bd84dd7acb15371291f3a08c8baf53a13f44a
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-03-27 13:53:58 +1100

    (feature) update sidepanel ui design

--- src/components/PreviewCard.vue
+++ src/components/PreviewCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="preview-card" class="h-28 w-full rounded-xl pt-4 pl-6 pr-6 pb-4 hover:shadow-lg">
+  <div id="preview-card" class="h-28 w-full rounded-xl pt-4 pl-6 pr-6 pb-4 mt-2 hover:shadow-lg">
     <div class="card-wrapper h-full flex flex-col justify-around">
       <div class="header h-4/16 flex flex-row justify-between items-center">
         <span class="title font-montserrat font-semibold tracking-tighter text-sm">
@@ -10,7 +10,7 @@
         </span>
       </div>
       <div class="card-body flex flex-row h-12/16 mt-1">
-        <span class="indent-bar h-full w-2 mr-3 rounded-md bg-green-400 opacity-50"/>
+        <span class="indent-bar h-full w-2 mr-3 rounded bg-green-400 opacity-50"/>
         <span class="body-content font-nunito-sans font-light tracking-tighter text-sm">
           {{ body }}
         </span>
--- src/components/home/SidePanel.vue
+++ src/components/home/SidePanel.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="side-panel" class="p-4">
-    <div class="content-wrapper rounded-xl w-full h-full pl-12 pr-12 pt-8 pb-8 bg-sidepanel flex flex-col justify-between border">
+    <div class="content-wrapper rounded-xl w-full h-full pl-8 pr-8 pt-8 pb-8 bg-sidepanel flex flex-col justify-between border">
       <div class="header flex flex-row justify-between h-14">
         <span class="panel-tabs flex flex-row w-6/16">
           <button class="profile-btn mr-8 font-nunito-sans font-bold text-gray-700 text-md"> Profile </button>
-<!--          <button class="settings-btn mr-8 font-nunito-sans font-bold text-gray-700 text-md"> Settings </button>-->
         </span>
         <img class="user-profile w-12 h-12 rounded-full" src="../../assets/my-portrait.png" alt="user-profile-photo"/>
       </div>
-      <div class="body-container flex-grow flex-col pt-16">
+      <div class="body-container flex-grow flex-col pt-10">
         <div class="greeting-container flex flex-row">
           <h1 class="greeting mr-2 font-roboto-slab text-3xl font-extrabold tracking-tight text-lighterblue"> Hey </h1>
           <img class="h-9 w-9 mr-2" src="../../assets/emoji-wave.png" alt="wave-emoji"/>
           <h1 class="greeting mr-2 font-roboto-slab text-3xl font-semibold tracking-tight text-lighterblue"> Edward! </h1>
         </div>
-        <recent id="recent" class="h-32 mt-12"/>
+        <recent id="recent" class="h-32 mt-10"/>
       </div>
-      <div class="footer flex flex-row justify-center h-14">
-        <button class="add-btn w-12 h-12 flex flex-wrap justify-center content-center rounded-2xl
-        filter drop-shadow-2xl bg-blue-500 hover:opacity-90 duration-300 ease-in-out">
-          <i class="ri-add-line" style="font-size: 28px; color: #FFFFFF"></i>
-        </button>
-      </div>
+<!--      <div class="footer flex flex-row justify-center h-14">-->
+<!--        <button class="add-btn w-12 h-12 flex flex-wrap justify-center content-center rounded-2xl-->
+<!--        filter drop-shadow-2xl bg-blue-500 hover:opacity-90 duration-300 ease-in-out">-->
+<!--          <i class="ri-add-line" style="font-size: 28px; color: #FFFFFF"></i>-->
+<!--        </button>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
