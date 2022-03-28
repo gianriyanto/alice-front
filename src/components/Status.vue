e32d7eb81a4909fc05a52567524c37f8d74a5bd2
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-03-28 20:13:25 +1100

    (feature) add status.vue component

---
+++ src/components/Status.vue
@@ -0,0 +1,21 @@
+<template>
+  <span id="status" class="flex flex-col w-14 h-4 pt-2 pb-2 justify-center bg-green-200 rounded-xl opacity-60 border border-green-300">
+    <span class="font-nunito-sans font-semibold tracking-tighter text-xs text-gray-800 m-auto">
+      {{ status }}
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Status",
+  props: [
+    "status"
+  ],
+  // TODO: set chip color based on status
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/explore/Channels.vue
+++ src/components/explore/Channels.vue
@@ -3,7 +3,7 @@
     <div class="channel-items flex flex-col h-full">
       <span class="item bg-gray-100 opacity-70 h-12 w-12 mt-4 rounded-lg border border-gray-300 hover:opacity-100 duration-300 ease-in-out"/>
       <span class="item bg-gray-100 opacity-70 h-12 w-12 mt-4 rounded-lg border border-gray-300 hover:opacity-100 duration-300 ease-in-out"/>
-      <span class="item bg-blue-200 opacity-70 h-12 w-12 mt-4 rounded-lg border border-blue-300 shadow-lg hover:opacity-100 duration-300 ease-in-out"/>
+      <span class="item bg-blue-300 opacity-90 h-12 w-12 mt-4 rounded-lg border border-blue-300 shadow-lg hover:opacity-100 duration-300 ease-in-out"/>
       <span class="item bg-gray-100 opacity-70 h-12 w-12 mt-4 rounded-lg border border-gray-300 hover:opacity-100 duration-300 ease-in-out"/>
     </div>
   </div>
