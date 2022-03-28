5dfe2187ae2d9c43b8ae042010d5f6d95466253b
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-03-28 20:27:48 +1100

    (feature) update SideBar.vue ui

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="side-bar">
-    <div class="content-wrapper h-full w-full flex flex-col items-center justify-between pt-11 pb-10">
+    <div class="content-wrapper h-full w-full flex flex-col items-center justify-between pt-8 pb-8">
       <div class="logo w-full justify-center flex">
-        <logo id="logo-1" class="w-14 h-14"/>
+        <logo id="logo-1" class="w-12 h-12"/>
       </div>
       <button class="logout hover:opacity-60 duration-200 ease-in-out">
         <i class="ri-logout-box-r-line" style="font-size: 25px; color: #323232"/>
--- src/components/explore/Thread.vue
+++ src/components/explore/Thread.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="thread" class="">
     <div class="thread-wrapper flex flex-col h-80 pl-4 pr-4 pt-4 pb-2 w-full rounded-xl border bg-white border-bluegrey">
-      <div class="header-container">
+      <div class="header-container flex flex-col">
         <status id="status" class="" status="open"/>
       </div>
       <div class="body-container">
