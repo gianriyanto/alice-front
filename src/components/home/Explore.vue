ceb25da8a5ddd17f3267bc4137ef3da7be0b22a6
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-03-29 22:48:39 +1100

    (feature) Adjust font and UI colour selection

--- src/components/home/Explore.vue
+++ src/components/home/Explore.vue
@@ -7,8 +7,8 @@
       <div class="explore-tags h-14/16 mt-0 flex flex-wrap">
         <span class="tag bg-blue-400 opacity-40 h-4 w-28 mr-2 mt-4 rounded-md hover:opacity-90 duration-300 ease-in-out"/>
         <span class="tag bg-green-200 opacity-50 h-4 w-28 mr-2 mt-4 rounded-md hover:opacity-80 duration-300 ease-in-out"/>
-        <span class="tag bg-indigo-300 opacity-50 h-4 w-32 mr-2 mt-4 rounded-md hover:opacity-90 duration-300 ease-in-out"/>
-        <span class="tag bg-indigo-300 opacity-50 h-4 w-36 mr-2 mt-4 rounded-md hover:opacity-80 duration-300 ease-in-out"/>
+        <span class="tag bg-slate300 opacity-50 h-4 w-32 mr-2 mt-4 rounded-md hover:opacity-90 duration-300 ease-in-out"/>
+        <span class="tag bg-blue-200 opacity-50 h-4 w-36 mr-2 mt-4 rounded-md hover:opacity-80 duration-300 ease-in-out"/>
         <span class="tag bg-blue-400 opacity-40 h-4 w-16 mr-2 mt-4 rounded-md hover:opacity-70 duration-300 ease-in-out"/>
         <span class="tag bg-pink-300 opacity-40 h-4 w-24 mr-2 mt-4 rounded-md hover:opacity-80 duration-300 ease-in-out"/>
       </div>
--- src/components/home/Trending.vue
+++ src/components/home/Trending.vue
@@ -5,13 +5,13 @@
         Trending
       </h3>
       <div class="trending-items h-14/16 mt-0 flex flex-wrap">
-        <span class="item bg-slate300 opacity-60 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
         <span class="item bg-blue-200 opacity-60 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
+        <span class="item bg-blue-300 opacity-20 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
         <span class="item bg-slate300 opacity-50 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
         <span class="item bg-blue-200 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
-        <span class="item bg-indigo-200 opacity-50 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
-        <span class="item bg-slate300 opacity-60 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
-        <span class="item bg-pink-300 opacity-30 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
+        <span class="item bg-slate200 opacity-70 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
+        <span class="item bg-blue-200 opacity-60 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
+        <span class="item bg-blue-200 opacity-30 h-20 w-20 mr-3 mt-4 rounded-md hover:opacity-100 duration-300 ease-in-out"/>
       </div>
     </div>
   </div>
