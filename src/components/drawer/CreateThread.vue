467288f3d71727d9c7f9cfb5e9669c534a14be3c
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-04-09 16:46:56 +1000

    (feature) Add buttons to CreateThread.vue

--- src/components/drawer/CreateThread.vue
+++ src/components/drawer/CreateThread.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="create-thread" class="flex flex-row pt-4 pb-4 pl-4 pr-2">
-    <div class="create-thread-wrapper flex flex-col w-full h-full pl-2 pr-4">
-      <div class="flex flex-col">
-        <h2 class="header text-xl text-gray-800 font-nunito-sans font-bold tracking-tight">
+    <div class="create-thread-wrapper flex flex-col w-full h-auto pl-2 pr-4">
+      <div class="header flex flex-col h-12">
+        <h2 class="title text-lg text-gray-800 font-nunito-sans
+                   font-extrabold tracking-tight">
           Create Thread
         </h2>
       </div>
-      <form class="form flex flex-col pt-12">
+      <form class="create-form flex flex-col">
         <span class="channel-input flex flex-col w-10/16">
           <label class="font-nunito-sans mb-2 text-xs font-bold text-gray-700">
             Channel
@@ -24,13 +25,14 @@
           <label class="font-nunito-sans mb-2 text-xs font-bold text-gray-700">
             Type
           </label>
-          <input id="inline-type"
-                 class="block appearance-none w-full bg-zinc200 bg-opacity-60 border rounded-sm
-                        border-zinc200 hover:border-zinc300 hover:bg-opacity-80 py-2 pl-2 pr-2
-                        font-nunito-sans text-sm text-gray-600 leading-tight
-                        rounded leading-tight focus:outline-none focus:shadow-outline"
-                 type="text"
-                 value="Question">
+            <select class="block appearance-none w-full bg-zinc200 bg-opacity-60 border rounded-sm
+                           border-zinc200 hover:border-zinc300 hover:bg-opacity-80 py-2 pl-2 pr-2
+                           font-nunito-sans text-sm text-gray-600 leading-tight
+                           rounded leading-tight focus:outline-none focus:shadow-outline">
+              <option>Question</option>
+              <option>Docs</option>
+              <option>Share</option>
+            </select>
         </span>
         <span class="Summary-input flex flex-col w-16/16 mt-10">
           <label class="font-nunito-sans mb-2 text-xs font-bold text-gray-700">
@@ -66,6 +68,24 @@
                  value="Tags">
         </span>
       </form>
+      <span class="footer flex flex-row mt-24 h-10 justify-end">
+        <span id="cancel-button"
+              class="flex flex-col mr-2 pl-8 pr-8 pt-2 pb-2 justify-center
+                     border border-zinc300 bg-zinc100 rounded-md bg-opacity-70
+                     hover:bg-zinc200 hover:bg-opacity-70 duration-300 ease-in-out">
+          <button class="font-nunito-sans font-bold text-gray-700 tracking-normal text-sm m-auto">
+            Cancel
+          </button>
+        </span>
+        <span id="create-button"
+              class="flex flex-col pl-8 pr-8 pt-2 pb-2 justify-center
+                     border rounded-md bg-green-500 bg-opacity-60
+                     hover:bg-opacity-80 duration-300 ease-in-out">
+          <button class="font-nunito-sans font-bold text-gray-700 tracking-normal text-sm m-auto">
+            Create
+          </button>
+        </span>
+      </span>
     </div>
   </div>
 </template>
