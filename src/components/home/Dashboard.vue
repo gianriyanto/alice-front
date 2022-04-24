4886596e2e2f21dde6ddb66af4149b4faec0c654
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-04-24 18:31:39 +1000

    (docs) add TODOs notification function

--- src/components/home/Dashboard.vue
+++ src/components/home/Dashboard.vue
@@ -3,12 +3,18 @@
     <div class="content-wrapper h-full h-24 pl-24 w-10/16 flex flex-col">
       <div class="header-container flex flex-col w-13/16">
         <div class="greeting-container flex flex-row mt-20">
-          <h1 class="greeting mr-2 font-roboto-slab text-4xl font-black tracking-tight text-slate800"> Hey </h1>
+          <h1 class="greeting mr-2 font-roboto-slab text-4xl font-black tracking-tight text-slate800">
+            Hey
+          </h1>
           <img class="h-9 mr-2 mt-2" src="../../assets/emoji-wave.png" alt="wave-emoji"/>
-          <h1 class="greeting mr-2 font-roboto-slab text-4xl font-extrabold tracking-tight text-slate800"> Edward! </h1>
+          <h1 class="greeting mr-2 font-roboto-slab text-4xl font-extrabold tracking-tight text-slate800">
+            Edward!
+          </h1>
         </div>
         <span class="message-container mt-2">
-          <h2 class="font-roboto-slab font-semibold tracking-normal text-md text-slate600"> You have 2 recently answered questions </h2>
+          <h2 class="font-roboto-slab font-semibold tracking-normal text-md text-slate600">
+            You have {{ notificationCount }} recently answered questions
+          </h2>
         </span>
       </div>
       <search id="search" class="h-8 w-full mt-12"/>
@@ -24,7 +30,12 @@ import Search from "./Search.vue";
 import Channels from "./Channels.vue";
 export default {
   name: "Dashboard",
-  components: {Channels, Search, Explore}
+  components: {Channels, Search, Explore},
+  data() {
+    return {
+      notificationCount: 2  // TODO: Listen for user notifications and dynamically display notification count
+    }
+  }
 }
 </script>
 
