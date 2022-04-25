b03ae27815b4d33b99c2dcf0eed2351884dcf92c
gianriyanto-hf <gian.riyanto@hellofresh.com.au>
2022-04-25 11:38:51 +1000

    (docs) fix tag chip font colour

--- src/components/explore/TagChip.vue
+++ src/components/explore/TagChip.vue
@@ -1,7 +1,7 @@
 <template>
-  <span class="tag-chip flex pt-0.5 pb-0.5 pl-3 pr-3 mt-3 rounded-xl border border-gray-400
+  <span class="tag-chip flex pt-0.5 pb-0.5 pl-3 pr-3 mt-3 rounded-md border border-gray-400
                hover:opacity-50 duration-200 ease-in-out cursor-pointer">
-    <span class="font-nunito-sans font-bold tracking-normal text-xs text-gray-500 m-auto">
+    <span class="font-nunito-sans font-bold tracking-normal text-xs text-gray-700 m-auto">
       {{ label }}
     </span>
   </span>
